7fd08437ff777953d1cec267968704d419e8f92e
Niko Gham <ng@nickgillham.co.uk>
2021-03-07 15:27:59 +0000

    form creation begin

---
+++ src/context/questions/QuestionState.js
@@ -0,0 +1,12 @@
+import React, {useReducer} from 'react'
+import QuestionContext from './questionContext'
+import questionReducer from './questionReducer'
+
+import {} from '../types'
+
+const QuestionState = (props) => {
+    const initialState = {
+        name: "",
+        email: ""
+    }
+}
